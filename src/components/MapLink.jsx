7ceabea24b304bc9a91c3b90189e84706c42cfcd
Romain Chauvin <rom.chauvin@gmail.com>
2025-02-25 11:06:29 +0100

    place id issue

--- src/components/MapLink.jsx
+++ src/components/MapLink.jsx
@@ -1,10 +1,15 @@
-// const address = "Florent Chauvin - Psychologue, 8 Rue du Ponceau, 95000 Cergy";
-const placeId = "ChIJpx-VQnP15kcRwRToKpSXUv4";
-const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=place_id:${placeId}`;
-const androidMapsUrl = `intent://maps.google.com/maps?query=place_id:${placeId}#Intent;scheme=https;package=com.google.android.apps.maps;end;`;
+const address = encodeURIComponent(
+  "Florent Chauvin - Psychologue, 8 Rue du Ponceau, 95000 Cergy, France",
+);
+const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${address}`;
+const androidMapsUrl = `intent://maps.google.com/maps?query=${address}#Intent;scheme=https;package=com.google.android.apps.maps;end;`;
+// const placeId = "ChIJpx-VQnP15kcRwRToKpSXUv4";
+// const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=place_id:${address}`;
+// const androidMapsUrl = `intent://maps.google.com/maps?query=place_id:${address}#Intent;scheme=https;package=com.google.android.apps.maps;end;`;
 
 const isAndroid = /Android/i.test(navigator.userAgent);
 const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+console.log(googleMapsUrl);
 
 export function MapLink({ children, className }) {
   const handleClick = (e) => {
@@ -23,7 +28,6 @@ export function MapLink({ children, className }) {
       onClick={handleClick}
       className={className}
     >
-      {navigator.userAgent}
       {children}
     </a>
   );
