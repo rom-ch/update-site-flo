abe2cbcdea0840cabc4a7d152b7487cda939adbe
Romain Chauvin <rom.chauvin@gmail.com>
2025-02-25 11:27:17 +0100

    fix address

--- src/components/MapLink.jsx
+++ src/components/MapLink.jsx
@@ -1,22 +1,27 @@
 const address = encodeURIComponent(
   "Florent Chauvin - Psychologue, 8 Rue du Ponceau, 95000 Cergy, France",
 );
-const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${address}`;
-const androidMapsUrl = `intent://maps.google.com/maps?query=${address}#Intent;scheme=https;package=com.google.android.apps.maps;end;`;
-// const placeId = "ChIJpx-VQnP15kcRwRToKpSXUv4";
-// const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=place_id:${address}`;
-// const androidMapsUrl = `intent://maps.google.com/maps?query=place_id:${address}#Intent;scheme=https;package=com.google.android.apps.maps;end;`;
 
+// Universal Google Maps URL (Fallback for desktops)
+const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${address}`;
+
+// Apple Maps URL for iOS
+const appleMapsUrl = `https://maps.apple.com/?q=${address}`;
+
+// Google Maps `geo:` URI for Android
+const androidMapsUrl = `geo:0,0?q=${address}`;
+
+// Detect device type
 const isAndroid = /Android/i.test(navigator.userAgent);
 const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-console.log(googleMapsUrl);
 
 export function MapLink({ children, className }) {
   const handleClick = (e) => {
     if (isAndroid) {
       window.location.href = androidMapsUrl;
+      e.preventDefault();
     } else if (isIOS) {
-      window.location.href = googleMapsUrl;
+      window.location.href = appleMapsUrl;
       e.preventDefault();
     }
   };
