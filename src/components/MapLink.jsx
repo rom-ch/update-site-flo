ab4e05f50d1b53bb6e320e0ecafbb245161a6915
Romain Chauvin <rom.chauvin@gmail.com>
2025-02-25 10:57:20 +0100

    map

--- src/components/MapLink.jsx
+++ src/components/MapLink.jsx
@@ -1,14 +1,17 @@
-const address = "Florent Chauvin - Psychologue, 8 Rue du Ponceau, 95000 Cergy";
+// const address = "Florent Chauvin - Psychologue, 8 Rue du Ponceau, 95000 Cergy";
+const placeId = "ChIJpx-VQnP15kcRwRToKpSXUv4";
+const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=place_id:${placeId}`;
+const androidMapsUrl = `intent://maps.google.com/maps?query=place_id:${placeId}#Intent;scheme=https;package=com.google.android.apps.maps;end;`;
+
+const isAndroid = /Android/i.test(navigator.userAgent);
+const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
 
 export function MapLink({ children, className }) {
-  const encodedAddredd = encodeURIComponent(address);
-  const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodedAddredd}`;
-  const mobileMapsUrl = `geo:0,0?q=${encodedAddredd}`;
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
   const handleClick = (e) => {
-    if (isMobile) {
-      window.location.href = mobileMapsUrl;
+    if (isAndroid) {
+      window.location.href = androidMapsUrl;
+    } else if (isIOS) {
+      window.location.href = googleMapsUrl;
       e.preventDefault();
     }
   };
