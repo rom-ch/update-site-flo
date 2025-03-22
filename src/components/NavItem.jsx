45f15972a81e28b12e8972d364aa65e4efbc4b1e
Romain Chauvin <rom.chauvin@gmail.com>
2025-03-22 13:19:28 +0100

    nav focus effect

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -3,7 +3,7 @@ export function NavItem({ name, href, setIsSidebarOpen }) {
     <li className="pr-8 pl-10 text-center sm:text-start lg:px-4">
       <a
         onClick={() => setIsSidebarOpen(false)}
-        className="relative block py-4 font-medium text-gray-700 after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-emerald-800 after:transition-[width] after:duration-300 hover:after:w-full focus:outline-none lg:py-3"
+        className="relative block py-4 font-medium text-gray-700 after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 after:bg-emerald-800 after:transition-[width] after:duration-300 hover:after:w-full focus:outline-none focus:after:w-full lg:py-3"
         href={href}
       >
         {name}
